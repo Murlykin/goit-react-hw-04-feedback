141fdb4ac06ef32c80fd250352ac1cf15266976d
Andrii Murlykin <109280143+Murlykin@users.noreply.github.com>
2023-03-04 15:32:24 +0200

    f

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,35 +1,38 @@
-import React, { Component }  from "react";
+import { useState } from "react";
 import Section from "components/Section/Section";
 import FeedbackOptions from 'components/FeedbackOptions/FeedbackOptions';
 import Statistics from 'components/Statistics/Statistics';
 import Notification from 'components/Notification/Notification';
 import {GlobalStyle} from 'components/GlobalStyle';
 
-class App extends Component {
-  state = {
+export default function App() {
+  const [feedbacks, setFeedbacks] = useState({
     good: 0,
     neutral: 0,
     bad: 0,
-  };
+  });
 
-  showFeedback = option => {
-    this.setState(prevState => ({
-      [option]: prevState[option] + 1,
+
+   const { good, neutral, bad } = feedbacks;
+
+  const showFeedback = option => {
+    setFeedbacks(prevState => ({
+      ...prevState, [option]: prevState[option] + 1
     }));
   };
 
-  countTotalFeedback() {
-    return this.state.good + this.state.neutral + this.state.bad;
+  function countTotalFeedback() {
+    return good + neutral + bad;
   };
     
-  countPositiveFeedbackPercentage = () => {
-    if (this.countTotalFeedback) {
-      return Math.round((this.state.good / this.countTotalFeedback()) * 100);
+  const countPositiveFeedbackPercentage = () => {
+    if (countTotalFeedback) {
+      return Math.round((good / countTotalFeedback()) * 100);
     }
     return 0;
   };
   
-  render() {
+
     return (
       
       <div
@@ -46,19 +49,19 @@ class App extends Component {
         <GlobalStyle />
         <Section title="Please leave feedback" >  
           <FeedbackOptions
-            options={Object.keys(this.state)}
-            onLeaveFeedback={this.showFeedback}
+            options={Object.keys(feedbacks)}
+            onLeaveFeedback={showFeedback}
           />{' '}
           </Section>
 
         <Section title="Statistics" > 
-          {this.countTotalFeedback() !== 0 ? (
+          {countTotalFeedback() !== 0 ? (
             <Statistics
-              good={this.state.good}
-              neutral={this.state.neutral}
-              bad={this.state.bad}
-              total={this.countTotalFeedback()}
-              positivePercentage={this.countPositiveFeedbackPercentage()}
+              good={good}
+              neutral={neutral}
+              bad={bad}
+              total={countTotalFeedback()}
+              positivePercentage={countPositiveFeedbackPercentage()}
             />
           ) : (<Notification message="There is no feedback"></Notification>)
           }
@@ -66,6 +69,5 @@ class App extends Component {
       </div>
     );
   };
-};
 
-export default App;
+
